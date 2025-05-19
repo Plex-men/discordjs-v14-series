23e82d040ed5995d9f69cf2a01342307d642ebe2
Plex-men <bobknow722022@gmail.com>
2025-05-18 23:35:44 -0400

    Update index.js

--- 16 - Balance Command/src/index.js
+++ 16 - Balance Command/src/index.js
@@ -1,28 +1,23 @@
-require('dotenv').config();
 const { Client, IntentsBitField } = require('discord.js');
-const mongoose = require('mongoose');
 const eventHandler = require('./handlers/eventHandler');
+require('dotenv/config');
 
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.GuildPresences,
     IntentsBitField.Flags.MessageContent,
   ],
 });
-
 (async () => {
   try {
-    mongoose.set('strictQuery', false);
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('Connected to DB.');
 
-    eventHandler(client);
+  eventHandler(client);
 
-    client.login(process.env.TOKEN);
-  } catch (error) {
-    console.log(`Error: ${error}`);
-  }
-})();
+    
+  client.login(process.env.TOKEN);
+} catch(error) {
+    console.log('Error: $(error)', error)
+    }
+});
